629ac8b9fa7328dd4f25f5d7429ec5357da53ac7
minghuaw <michael.wu1107@gmail.com>
2023-12-22 17:17:05 -0800

    test generating binding to extras.h

---
+++ mlx-sys/src/extras.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <sstream>
+
+// autocxx isn't yet smart enough to do anything with the R2Point
+// structure, so here we've manually made a cheeky little API to
+// do something useful with it.
+inline std::string hello() {
+    std::ostringstream oss;
+    oss << "hello";
+    return oss.str();
+}
--- mlx-sys/src/lib.rs
+++ mlx-sys/src/lib.rs
@@ -1,5 +1,17 @@
 use autocxx::prelude::*;
 
 include_cpp! {
-    
+    #include "extras.h"
+    // TODO: what safety option should be used here?
+    safety!(unsafe)
+    generate!("hello")
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn extras_hello_works() {
+        let hello = super::ffi::hello();
+        println!("ffi::hello: {}", hello);
+    }
 }

3fff8eee3ff86b0d8568872df6a27425611d6b84
minghuaw <michael.wu1107@gmail.com>
2023-12-26 00:35:55 -0800

    change modifier to build script

--- mlx-sys/build.rs
+++ mlx-sys/build.rs
@@ -2,8 +2,8 @@ fn main() -> miette::Result<()> {
     let dst = cmake::Config::new("mlx").build();
     println!("cargo:warning={}", dst.display());
 
-    println!("cargo:rustc-link-search={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=mlx");
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=static=mlx");
 
     Ok(())
 }

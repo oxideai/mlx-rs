0d3efa83d4aec3cdbc83620ed3cb2002932f7a72
minghuaw <michael.wu1107@gmail.com>
2023-12-26 05:03:24 -0800

    same linker error

--- mlx-sys/build.rs
+++ mlx-sys/build.rs
@@ -1,16 +1,9 @@
-fn main() -> miette::Result<()> {
-    // It's necessary to use an absolute path here because the
-    // C++ codegen and the macro codegen appears to be run from different
-    // working directories.
-    let path = std::path::PathBuf::from("mlx");
-    let path2 = std::path::PathBuf::from("src");
-    let mut b = autocxx_build::Builder::new("src/main.rs", &[&path, &path2])
-        .extra_clang_args(&["-std=c++17"])
-        .build()?;
-    b
-        .cpp(true)
+fn main() {
+    cxx_build::bridge("src/main.rs")
+        // .file("mlx/mlx/random.cpp")
+        .include("mlx")
         .flag_if_supported("-std=c++17")
-        .compile("mlx-binding");
+        .compile("random");
+
     println!("cargo:rerun-if-changed=src/main.rs");
-    Ok(())
 }
--- mlx-sys/src/main.rs
+++ mlx-sys/src/main.rs
@@ -1,74 +1,14 @@
-use autocxx::prelude::*;
 
-include_cpp! {
-    #include "mlx/mlx.h"
-    #include "ext.h"
-    // #include "extras.h" // TODO: remove this later
-    // TODO: what safety option should be used here?
-    safety!(unsafe)
+#[cxx::bridge(namespace = "mlx")]
+mod ffi {
+    unsafe extern "C++" {
+        include!("mlx/mlx.h");
 
-    generate!("ext::hello")
-    generate_ns!("ext::array")
-
-    // mlx/mlx/allocator.h
-    generate!("mlx::core::allocator::Buffer")
-    
-    // mlx/mlx/dtype.h
-    generate!("mlx::core::Dtype")
-    generate!("mlx::core::is_available")
-    generate!("mlx::core::promote_types")
-    generate!("mlx::core::kindof")
-    generate!("mlx::core::is_unsigned")
-    generate!("mlx::core::is_floating_point")
-    generate!("mlx::core::is_complex")
-    generate!("mlx::core::is_integral")
-    generate!("mlx::core::dtype_to_array_protocol")
-    generate!("mlx::core::dtype_from_array_protocol")
-    
-    // mlx/mlx/backend/metal/metal.h
-    generate_ns!("mlx::core::metal")
-    
-    // mlx/mlx/device.h
-    generate!("mlx::core::Device")
-    generate!("mlx::core::set_default_device")
-    
-    // mlx/mlx/fft.h
-    generate_ns!("mlx::core::fft")
-
-    // mlx/mlx/ops.h
-    // TODO:
-
-    // mlx/mlx/random.h
-    generate_ns!("mlx::core::random")
-
-    // mlx/mlx/stream.h
-    generate!("mlx::core::Stream")
-    generate!("mlx::core::default_stream")
-    generate!("mlx::core::set_default_stream")
-    generate!("mlx::core::new_stream")
-
-    // mlx/mlx/transforms.h
-    // TODO:
-    // generate!("mlx::core::simplify")
-
-    // mlx/mlx/utils.h
-    generate!("mlx::core::result_type")
-    generate!("mlx::core::broadcast_shapes")
-    generate!("mlx::core::is_same_shape")
-    generate!("mlx::core::normalize_axis")
-
-    generate!("mlx::core::deleter_t") // TODO: somehow this is fine
-
-    // TODO: Unsupported
-    // generate!("mlx::core::TypeToDtype") // TODO: template specialization, see concrete!
-    // generate!("mlx::core::default_device") // bindings cannot be generated
-    // generate!("mlx::core::array") // TODO: array is not supported by autocxx
-
+        #[namespace = "mlx::core::random"]
+        fn seed(seed: u64);
+    }
 }
 
-pub use ffi::*;
-
 fn main() {
-    // let key = mlx::core::random::key(1);
-    let key = mlx::core::random::key(1).within_box();
+    ffi::seed(1)
 }

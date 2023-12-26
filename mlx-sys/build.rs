09c9dc9c78ffb2a68f36bc85bd9c2360eab4bf3b
minghuaw <michael.wu1107@gmail.com>
2023-12-26 00:08:40 -0800

    cmake build and link works

--- mlx-sys/build.rs
+++ mlx-sys/build.rs
@@ -1,16 +1,9 @@
 fn main() -> miette::Result<()> {
-    // It's necessary to use an absolute path here because the
-    // C++ codegen and the macro codegen appears to be run from different
-    // working directories.
-    let path = std::path::PathBuf::from("mlx");
-    let path2 = std::path::PathBuf::from("src");
-    let mut b = autocxx_build::Builder::new("src/main.rs", &[&path, &path2])
-        .extra_clang_args(&["-std=c++17"])
-        .build()?;
-    b
-        .cpp(true)
-        .flag_if_supported("-std=c++17")
-        .compile("mlx-binding");
-    println!("cargo:rerun-if-changed=src/main.rs");
+    let dst = cmake::Config::new("mlx").build();
+    println!("cargo:warning={}", dst.display());
+
+    println!("cargo:rustc-link-search={}/lib", dst.display());
+    println!("cargo:rustc-link-lib=mlx");
+
     Ok(())
 }

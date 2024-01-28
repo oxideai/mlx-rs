a14df909a102091b2d2ff570812109e47e40270e
minghuaw <michael.wu1107@gmail.com>
2024-01-28 06:57:59 -0800

    added unit tests for device

--- mlx-sys/src/device.rs
+++ mlx-sys/src/device.rs
@@ -25,6 +25,9 @@ pub mod ffi {
         #[namespace = "mlx::core"]
         type Device;
 
+        #[namespace = "mlx_cxx"]
+        fn new_device(device_type: DeviceType, index: i32) -> Device;
+
         #[namespace = "mlx::core"]
         fn default_device() -> &'static Device;
 
@@ -39,5 +42,22 @@ mod tests {
     fn test_default_deveice() {
         let device = super::ffi::default_device();
         println!("{:?}", device.index);
+    }
+
+    #[test]
+    fn test_new_device() {
+        let device = super::ffi::new_device(super::ffi::DeviceType::gpu, 0);
+        println!("{:?}", device.index);
+    }
+
+    #[test]
+    fn test_default_device() {
+        let _device = super::ffi::default_device();
+    }
+
+    #[test]
+    fn test_set_default_device() {
+        let device = super::ffi::new_device(super::ffi::DeviceType::gpu, 0);
+        super::ffi::set_default_device(&device);
     }
 }

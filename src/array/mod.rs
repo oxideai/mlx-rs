5e897664dd9d5f489982a82a82b45c8a15ab402c
dcvz <david@dcvz.io>
2024-04-18 15:45:41 +0200

    Add shape

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -1,3 +1,5 @@
+use crate::array::shape::Shape;
+
 mod kind;
 pub mod ops;
 mod shape;
@@ -13,7 +15,23 @@ impl<E: kind::Element, const D: usize> MLXArray<E, D> {
         self.tensor.eval();
     }
 
+    pub fn shape(&self) -> Shape<D> {
+        Shape::from(self.tensor.shape())
+    }
+
     pub fn as_slice(&self) -> Option<&[E]> {
         self.tensor.as_slice()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::stream::StreamOrDevice;
+
+    #[test]
+    fn test_shape() {
+        let array: MLXArray<f32, 2> = MLXArray::zeros([2, 3], StreamOrDevice::default());
+        assert_eq!(array.shape().dims, [2, 3]);
     }
 }

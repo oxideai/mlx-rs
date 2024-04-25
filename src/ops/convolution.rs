af54a3a095c6b379ac1515914f283f73cabdca09
dcvz <david@dcvz.io>
2024-04-25 16:39:51 +0200

    Address PR comments

--- src/ops/convolution.rs
+++ src/ops/convolution.rs
@@ -38,16 +38,15 @@ fn conv_general_device_inner(
 
 /// General convolution over an input with several channels.
 ///
-/// > Only 1d and 2d convolutions are supported at the moment
-///
-/// > the default `groups: 1` is currently supported
+/// - Only 1d and 2d convolutions are supported at the moment
+/// - the default `groups: 1` is currently supported
 ///
 /// # Params
 /// - array: Input array of shape `&[N, ..., C_in]`
 /// - weight: Weight array of shape `&[C_out, ..., C_in]`
 /// - strides: The kernel strides. All dimensions get the same stride if only one number is specified.
 /// - padding: The input padding. All dimensions get the same padding if only one number is specified.
-/// - kernel_dilation: `The  kernel dilation. All dimensions get the same dilation if only one number is specified.
+/// - kernel_dilation: The kernel dilation. All dimensions get the same dilation if only one number is specified.
 /// - input_dilation: The input dilation. All dimensions get the same dilation if only one number is specified.
 /// - groups: Input feature groups
 /// - flip: Flip the order in which the spatial dimensions of the weights are processed.
@@ -82,16 +81,15 @@ pub fn conv_general_device<'a>(
 
 /// General convolution over an input with several channels without validating the inputs.
 ///
-/// > Only 1d and 2d convolutions are supported at the moment
-///
-/// > the default `groups: 1` is currently supported
+/// - Only 1d and 2d convolutions are supported at the moment
+/// - the default `groups: 1` is currently supported
 ///
 /// # Params
 /// - array: Input array of shape `&[N, ..., C_in]`
 /// - weight: Weight array of shape `&[C_out, ..., C_in]`
 /// - strides: The kernel strides. All dimensions get the same stride if only one number is specified.
 /// - padding: The input padding. All dimensions get the same padding if only one number is specified.
-/// - kernel_dilation: `The  kernel dilation. All dimensions get the same dilation if only one number is specified.
+/// - kernel_dilation: The kernel dilation. All dimensions get the same dilation if only one number is specified.
 /// - input_dilation: The input dilation. All dimensions get the same dilation if only one number is specified.
 /// - groups: Input feature groups
 /// - flip: Flip the order in which the spatial dimensions of the weights are processed.
@@ -101,14 +99,12 @@ pub fn conv_general_device<'a>(
 ///
 /// # Safety
 ///
-/// This function is unsafe because it does not validate the inputs.
-///
-///  # Panic
-/// - Panic if the groups are not equal to 1.
-/// - Panic if the input and weight arrays do not have the same number of input channels.
-/// - Panic if the input and weight arrays do not have the same number of spatial dimensions.
-/// - Panic if arrays are not floating point arrays.
-/// - Panic if arrays are not 1d or 2d arrays.
+/// This function is unsafe because it does not validate:
+/// - if the groups are not equal to 1
+/// - if the input and weight arrays do not have the same number of input channels
+/// - if the input and weight arrays do not have the same number of spatial dimensions
+/// - if arrays are not floating point arrays
+/// - if arrays are not 1d or 2d arrays
 #[default_device]
 pub unsafe fn conv_general_device_unchecked<'a>(
     array: &Array,
@@ -144,16 +140,15 @@ pub unsafe fn conv_general_device_unchecked<'a>(
 
 /// General convolution over an input with several channels returning an error if the inputs are invalid.
 ///
-/// > Only 1d and 2d convolutions are supported at the moment
-///
-/// > the default `groups: 1` is currently supported
+/// - Only 1d and 2d convolutions are supported at the moment
+/// - the default `groups: 1` is currently supported
 ///
 /// # Params
 /// - array: Input array of shape `&[N, ..., C_in]`
 /// - weight: Weight array of shape `&[C_out, ..., C_in]`
 /// - strides: The kernel strides. All dimensions get the same stride if only one number is specified.
 /// - padding: The input padding. All dimensions get the same padding if only one number is specified.
-/// - kernel_dilation: `The  kernel dilation. All dimensions get the same dilation if only one number is specified.
+/// - kernel_dilation: The kernel dilation. All dimensions get the same dilation if only one number is specified.
 /// - input_dilation: The input dilation. All dimensions get the same dilation if only one number is specified.
 /// - groups: Input feature groups
 /// - flip: Flip the order in which the spatial dimensions of the weights are processed.
@@ -200,7 +195,7 @@ pub fn try_conv_general_device<'a>(
         ));
     }
 
-    if !array.dtype().is_floating() {
+    if !array.dtype().is_float() {
         return Err(OperationError::WrongInput(
             "Only floating point arrays are supported".to_string(),
         ));
@@ -218,7 +213,7 @@ pub fn try_conv_general_device<'a>(
         ));
     }
 
-    if array.shape()[spatial_dims + 1] != weight.shape()[spatial_dims + 1] {
+    if  array.shape().get(spatial_dims + 1) != weight.shape().get(spatial_dims + 1) {
         return Err(OperationError::WrongDimensions(
             "Input and weight arrays must have the same number of input channels".to_string(),
         ));

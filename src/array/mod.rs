8a9495947c93a320c161328a23120a88e0ffd1e0
dcvz <david@dcvz.io>
2024-05-05 00:00:19 +0200

    Add PartialEq conformance

--- src/array/mod.rs
+++ src/array/mod.rs
@@ -42,6 +42,19 @@ impl Drop for Array {
     }
 }
 
+impl PartialEq for &Array {
+    /// Array equality check.
+    ///
+    /// Compare two arrays for equality. Returns `true` iff the arrays have
+    /// the same shape and their values are equal. The arrays need not have
+    /// the same type to be considered equal.
+    ///
+    /// If you're looking for element-wise equality, use the [Array::eq()] method.
+    fn eq(&self, other: &Self) -> bool {
+        self.array_eq(other, None).item()
+    }
+}
+
 impl Array {
     /// Create a new array from an existing mlx_array pointer.
     ///
@@ -398,5 +411,16 @@ mod tests {
         assert_eq!(array.dim(-2), 2); // negative index
         assert_eq!(array.shape(), &[2, 3]);
         assert_eq!(array.dtype(), Dtype::Int32);
+    }
+
+    #[test]
+    fn test_array_eq() {
+        let data = [1i32, 2, 3, 4, 5];
+        let array1 = Array::from_slice(&data, &[5]);
+        let array2 = Array::from_slice(&data, &[5]);
+        let array3 = Array::from_slice(&[1i32, 2, 3, 4, 6], &[5]);
+
+        assert_eq!(&array1, &array2);
+        assert_ne!(&array1, &array3);
     }
 }

de03320089800e7c2b8a92e6d5498c4bcb801d0e
David Chavez <david@dcvz.io>
2024-05-06 10:07:52 +0200

    feat(api): Add assign variants of Array operators (#58)

--- src/array/operators.rs
+++ src/array/operators.rs
@@ -1,6 +1,6 @@
 use crate::{Array, StreamOrDevice};
 use num_traits::Pow;
-use std::ops::{Add, Div, Mul, Neg, Rem, Sub};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, Sub, SubAssign};
 
 macro_rules! impl_binary_op {
     ($trait:ident, $method:ident) => {
@@ -24,10 +24,36 @@ macro_rules! impl_binary_op {
     };
 }
 
+macro_rules! impl_binary_op_assign {
+    ($trait:ident, $method:ident, $c_method:ident) => {
+        impl<T: Into<Array>> $trait<T> for Array {
+            fn $method(&mut self, rhs: T) {
+                let new_array = paste::paste! {
+                    self.[<$c_method _device>](&rhs.into(), StreamOrDevice::default())
+                };
+                *self = new_array;
+            }
+        }
+
+        impl $trait<&Array> for Array {
+            fn $method(&mut self, rhs: &Self) {
+                let new_array = paste::paste! {
+                    self.[<$c_method _device>](rhs, StreamOrDevice::default())
+                };
+                *self = new_array;
+            }
+        }
+    };
+}
+
 impl_binary_op!(Add, add);
+impl_binary_op_assign!(AddAssign, add_assign, add);
 impl_binary_op!(Sub, sub);
+impl_binary_op_assign!(SubAssign, sub_assign, sub);
 impl_binary_op!(Mul, mul);
+impl_binary_op_assign!(MulAssign, mul_assign, mul);
 impl_binary_op!(Div, div);
+impl_binary_op_assign!(DivAssign, div_assign, div);
 impl_binary_op!(Rem, rem);
 
 impl<'a> Pow<&'a Array> for &'a Array {
@@ -48,5 +74,51 @@ impl<'a> Neg for &'a Array {
     type Output = Array;
     fn neg(self) -> Self::Output {
         self.logical_not()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_add_assign() {
+        let mut a = Array::from_slice(&[1.0, 2.0, 3.0], &[3]);
+        let b = Array::from_slice(&[4.0, 5.0, 6.0], &[3]);
+        a += &b;
+        a.eval();
+
+        assert_eq!(a.as_slice::<f32>(), &[5.0, 7.0, 9.0]);
+    }
+
+    #[test]
+    fn test_sub_assign() {
+        let mut a = Array::from_slice(&[1.0, 2.0, 3.0], &[3]);
+        let b = Array::from_slice(&[4.0, 5.0, 6.0], &[3]);
+        a -= &b;
+        a.eval();
+
+        assert_eq!(a.as_slice::<f32>(), &[-3.0, -3.0, -3.0]);
+    }
+
+    #[test]
+    fn test_mul_assign() {
+        let mut a = Array::from_slice(&[1.0, 2.0, 3.0], &[3]);
+        let b = Array::from_slice(&[4.0, 5.0, 6.0], &[3]);
+        a *= &b;
+        a.eval();
+
+        assert_eq!(a.as_slice::<f32>(), &[4.0, 10.0, 18.0]);
+    }
+
+    #[test]
+    fn test_div_assign() {
+        let mut a = Array::from_slice(&[1.0, 2.0, 3.0], &[3]);
+        let b = Array::from_slice(&[4.0, 5.0, 6.0], &[3]);
+        a /= &b;
+        a.eval();
+
+        assert_eq!(a.as_slice::<f32>(), &[0.25, 0.4, 0.5]);
     }
 }

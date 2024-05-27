f1cffc5ac71bc529316b5dfa2c3384ed1a550094
minghuaw <michael.wu1107@gmail.com>
2024-05-27 09:38:38 -0700

    added rfft macros

--- src/fft/macros.rs
+++ src/fft/macros.rs
@@ -94,6 +94,102 @@ macro_rules! ifftn {
     };
 }
 
+#[macro_export]
+macro_rules! rfft {
+    ($a:expr) => {
+        $crate::fft::rfft($a, None, None)
+    };
+    ($a:expr, $n:expr) => {
+        $crate::fft::rfft($a, $n, None)
+    };
+    ($a:expr, $n:expr, $axis:expr) => {
+        $crate::fft::rfft($a, $n, $axis)
+    };
+    ($a:expr, $n:expr, $axis:expr, $stream:expr) => {
+        $crate::fft::rfft_device($a, $n, $axis, $stream)
+    };
+}
+
+#[macro_export]
+macro_rules! rfft2 {
+    ($a:expr) => {
+        $crate::fft::rfft2($a, None, None)
+    };
+    ($a:expr, $shape:expr) => {
+        $crate::fft::rfft2($a, $shape, None)
+    };
+    ($a:expr, $shape:expr, $axes:expr) => {
+        $crate::fft::rfft2($a, $shape, $axes)
+    };
+    ($a:expr, $shape:expr, $axes:expr, $stream:expr) => {
+        $crate::fft::rfft2_device($a, $shape, $axes, $stream)
+    };
+}
+
+#[macro_export]
+macro_rules! rfftn {
+    ($a:expr) => {
+        $crate::fft::rfftn($a, None, None)
+    };
+    ($a:expr, $shape:expr) => {
+        $crate::fft::rfftn($a, $shape, None)
+    };
+    ($a:expr, $shape:expr, $axes:expr) => {
+        $crate::fft::rfftn($a, $shape, $axes)
+    };
+    ($a:expr, $shape:expr, $axes:expr, $stream:expr) => {
+        $crate::fft::rfftn_device($a, $shape, $axes, $stream)
+    };
+}
+
+#[macro_export]
+macro_rules! irfft {
+    ($a:expr) => {
+        $crate::fft::irfft($a, None, None)
+    };
+    ($a:expr, $n:expr) => {
+        $crate::fft::irfft($a, $n, None)
+    };
+    ($a:expr, $n:expr, $axis:expr) => {
+        $crate::fft::irfft($a, $n, $axis)
+    };
+    ($a:expr, $n:expr, $axis:expr, $stream:expr) => {
+        $crate::fft::irfft_device($a, $n, $axis, $stream)
+    };
+}
+
+#[macro_export]
+macro_rules! irfft2 {
+    ($a:expr) => {
+        $crate::fft::irfft2($a, None, None)
+    };
+    ($a:expr, $shape:expr) => {
+        $crate::fft::irfft2($a, $shape, None)
+    };
+    ($a:expr, $shape:expr, $axes:expr) => {
+        $crate::fft::irfft2($a, $shape, $axes)
+    };
+    ($a:expr, $shape:expr, $axes:expr, $stream:expr) => {
+        $crate::fft::irfft2_device($a, $shape, $axes, $stream)
+    };
+}
+
+#[macro_export]
+macro_rules! irfftn {
+    ($a:expr) => {
+        $crate::fft::irfftn($a, None, None)
+    };
+    ($a:expr, $shape:expr) => {
+        $crate::fft::irfftn($a, $shape, None)
+    };
+    ($a:expr, $shape:expr, $axes:expr) => {
+        $crate::fft::irfftn($a, $shape, $axes)
+    };
+    ($a:expr, $shape:expr, $axes:expr, $stream:expr) => {
+        $crate::fft::irfftn_device($a, $shape, $axes, $stream)
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{Array, StreamOrDevice};
@@ -118,32 +214,103 @@ mod tests {
         assert_eq!(a, ifft_a);
     }
 
-    // #[test]
-    // fn test_fft2() {
-    //     let a = Array::from_slice(&[1.0, 2.0, 3.0, 4.0], &[2, 2]);
-    //     let fft2_a = fft2!(&a);
-    //     let ifft2_a = ifft2!(&fft2_a);
-    //     assert_eq!(a, ifft2_a);
+    #[test]
+    fn test_fft2() {
+        let a = Array::from_slice(&[1.0, 2.0, 3.0, 4.0], &[2, 2]);
+        let fft2_a = fft2!(&a);
+        let ifft2_a = ifft2!(&fft2_a);
+        assert_eq!(a, ifft2_a);
 
-    //     let fft2_a = fft2!(&a, &[2, 2][..]);
-    //     let ifft2_a = ifft2!(&fft2_a, &[2, 2][..]);
-    //     assert_eq!(a, ifft2_a);
+        let fft2_a = fft2!(&a, &[2, 2][..]);
+        let ifft2_a = ifft2!(&fft2_a, &[2, 2][..]);
+        assert_eq!(a, ifft2_a);
 
-    //     let fft2_a = fft2!(&a, &[2, 2][..], &[0, 1][..]);
-    //     let ifft2_a = ifft2!(&fft2_a, &[2i32, 2], &[0, 1]);
-    //     assert_eq!(a, ifft2_a);
+        let fft2_a = fft2!(&a, &[2, 2][..], &[0, 1][..]);
+        let ifft2_a = ifft2!(&fft2_a, &[2, 2][..], &[0, 1][..]);
+        assert_eq!(a, ifft2_a);
 
-    //     let fft2_a = fft2!(&a, &[2, 2], &[0, 1], StreamOrDevice::cpu());
-    //     let ifft2_a = ifft2!(&fft2_a, &[2, 2], &[0, 1], StreamOrDevice::cpu());
-    //     assert_eq!(a, ifft2_a);
-    // }
+        let fft2_a = fft2!(&a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        let ifft2_a = ifft2!(&fft2_a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        assert_eq!(a, ifft2_a);
+    }
 
-    // #[test]
-    // fn test_fftn() {
-    //     let a = Array::from_slice(&[1.0, 2.0, 3.0, 4.0], &[2, 2]);
-    //     let _result = fftn!(&a);
-    //     let _result = fftn!(&a, &[4][..]);
-    //     let _result = fftn!(&a, &[4][..], &[0][..]);
-    //     let _result = fftn!(&a, &[4][..], &[0][..], StreamOrDevice::default());
-    // }
+    #[test]
+    fn test_fftn() {
+        let a = Array::from_slice(&[1.0, 2.0, 3.0, 4.0], &[2, 2]);
+        let fftn_a = fftn!(&a);
+        let ifftn_a = ifftn!(&fftn_a);
+        assert_eq!(a, ifftn_a);
+
+        let fftn_a = fftn!(&a, &[2, 2][..]);
+        let ifftn_a = ifftn!(&fftn_a, &[2, 2][..]);
+        assert_eq!(a, ifftn_a);
+
+        let fftn_a = fftn!(&a, &[2, 2][..], &[0, 1][..]);
+        let ifftn_a = ifftn!(&fftn_a, &[2, 2][..], &[0, 1][..]);
+        assert_eq!(a, ifftn_a);
+
+        let fftn_a = fftn!(&a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        let ifftn_a = ifftn!(&fftn_a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        assert_eq!(a, ifftn_a);
+    }
+
+    #[test]
+    fn test_rfft() {
+        let a = Array::from_slice(&[1.0, 2.0, 3.0, 4.0], &[4]);
+        let rfft_a = rfft!(&a);
+        let irfft_a = irfft!(&rfft_a);
+        assert_eq!(a, irfft_a);
+
+        let rfft_a = rfft!(&a, 4);
+        let irfft_a = irfft!(&rfft_a, 4);
+        assert_eq!(a, irfft_a);
+
+        let rfft_a = rfft!(&a, 4, 0);
+        let irfft_a = irfft!(&rfft_a, 4, 0);
+        assert_eq!(a, irfft_a);
+
+        let rfft_a = rfft!(&a, 4, 0, StreamOrDevice::cpu());
+        let irfft_a = irfft!(&rfft_a, 4, 0, StreamOrDevice::cpu());
+        assert_eq!(a, irfft_a);
+    }
+
+    #[test]
+    fn test_rfft2() {
+        let a = Array::from_slice(&[1.0, 2.0, 3.0, 4.0], &[2, 2]);
+        let rfft2_a = rfft2!(&a);
+        let irfft2_a = irfft2!(&rfft2_a);
+        assert_eq!(a, irfft2_a);
+
+        let rfft2_a = rfft2!(&a, &[2, 2][..]);
+        let irfft2_a = irfft2!(&rfft2_a, &[2, 2][..]);
+        assert_eq!(a, irfft2_a);
+
+        let rfft2_a = rfft2!(&a, &[2, 2][..], &[0, 1][..]);
+        let irfft2_a = irfft2!(&rfft2_a, &[2, 2][..], &[0, 1][..]);
+        assert_eq!(a, irfft2_a);
+
+        let rfft2_a = rfft2!(&a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        let irfft2_a = irfft2!(&rfft2_a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        assert_eq!(a, irfft2_a);
+    }
+
+    #[test]
+    fn test_rfftn() {
+        let a = Array::from_slice(&[1.0, 2.0, 3.0, 4.0], &[2, 2]);
+        let rfftn_a = rfftn!(&a);
+        let irfftn_a = irfftn!(&rfftn_a);
+        assert_eq!(a, irfftn_a);
+
+        let rfftn_a = rfftn!(&a, &[2, 2][..]);
+        let irfftn_a = irfftn!(&rfftn_a, &[2, 2][..]);
+        assert_eq!(a, irfftn_a);
+
+        let rfftn_a = rfftn!(&a, &[2, 2][..], &[0, 1][..]);
+        let irfftn_a = irfftn!(&rfftn_a, &[2, 2][..], &[0, 1][..]);
+        assert_eq!(a, irfftn_a);
+
+        let rfftn_a = rfftn!(&a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        let irfftn_a = irfftn!(&rfftn_a, &[2, 2][..], &[0, 1][..], StreamOrDevice::cpu());
+        assert_eq!(a, irfftn_a);
+    }
 }

e71e3bb9e83c29ab0238e385b29ac76d6696c353
minghuaw <michael.wu1107@gmail.com>
2024-06-10 04:45:38 -0700

    fix mod macros in merge

--- src/macros.rs
+++ src/macros.rs
@@ -1,11 +1,2 @@
-//! All internal macros
-
-/// See `assertEqual` in the swift binding tests
-#[allow(unused_macros)]
-macro_rules! assert_array_all_close {
-    ($a:tt, $b:tt) => {
-        let _b: Array = $b.into();
-        let mut assert = $a.all_close(&_b, None, None, None);
-        assert!(assert.item::<bool>());
-    };
-}
+#[macro_use]
+mod internal;

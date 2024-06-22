f369214b689d17bdd6912a7b75c5c1e7c19b66f3
minghuaw <michael.wu1107@gmail.com>
2024-06-22 12:41:58 -0700

    added diag tests

--- src/ops/other.rs
+++ src/ops/other.rs
@@ -1,6 +1,6 @@
 use mlx_macros::default_device;
 
-use crate::{Array, Stream, StreamOrDevice};
+use crate::{error::Exception, Array, Stream, StreamOrDevice};
 
 impl Array {
     /// Extract a diagonal or construct a diagonal matrix.
@@ -9,17 +9,25 @@ impl Array {
     /// self is 2-D then the `k`-th diagonal is returned.
     ///
     /// # Params:
-    /// 
+    ///
     /// - `k`: the diagonal to extract or construct
     /// - `stream`: stream or device to evaluate on
     #[default_device]
-    pub fn diag_device(&self, k: impl Into<Option<i32>>, stream: impl AsRef<Stream>) -> Array {
+    pub fn diag_device(
+        &self,
+        k: impl Into<Option<i32>>,
+        stream: impl AsRef<Stream>,
+    ) -> Result<Array, Exception> {
         unsafe {
-            Array::from_ptr(mlx_sys::mlx_diag(
-                self.c_array,
-                k.into().unwrap_or(0),
-                stream.as_ref().as_ptr(),
-            ))
+            let c_array = try_catch_c_ptr_expr! {
+                mlx_sys::mlx_diag(
+                    self.c_array,
+                    k.into().unwrap_or(0),
+                    stream.as_ref().as_ptr(),
+                )
+            };
+
+            Ok(Array::from_ptr(c_array))
         }
     }
 
@@ -32,7 +40,7 @@ impl Array {
     /// `axis2` removed and a new dimension inserted at the end corresponding to the diagonal.
     ///
     /// # Params:
-    /// 
+    ///
     /// - `offset`: offset of the diagonal.  Can be positive or negative
     /// - `axis1`: first axis of the 2-D sub-array from which the diagonals should be taken
     /// - `axis2`: second axis of the 2-D sub-array from which the diagonals should be taken
@@ -44,22 +52,30 @@ impl Array {
         axis1: impl Into<Option<i32>>,
         axis2: impl Into<Option<i32>>,
         stream: impl AsRef<Stream>,
-    ) -> Array {
+    ) -> Result<Array, Exception> {
         unsafe {
-            Array::from_ptr(mlx_sys::mlx_diagonal(
-                self.c_array,
-                offset.into().unwrap_or(0),
-                axis1.into().unwrap_or(0),
-                axis2.into().unwrap_or(1),
-                stream.as_ref().as_ptr(),
-            ))
+            let c_array = try_catch_c_ptr_expr! {
+                mlx_sys::mlx_diagonal(
+                    self.c_array,
+                    offset.into().unwrap_or(0),
+                    axis1.into().unwrap_or(0),
+                    axis2.into().unwrap_or(1),
+                    stream.as_ref().as_ptr(),
+                )
+            };
+
+            Ok(Array::from_ptr(c_array))
         }
     }
 }
 
 /// See [`Array::diag`]
 #[default_device]
-pub fn diag_device(a: &Array, k: impl Into<Option<i32>>, stream: impl AsRef<Stream>) -> Array {
+pub fn diag_device(
+    a: &Array,
+    k: impl Into<Option<i32>>,
+    stream: impl AsRef<Stream>,
+) -> Result<Array, Exception> {
     a.diag_device(k, stream)
 }
 
@@ -71,6 +87,112 @@ pub fn diagonal_device(
     axis1: impl Into<Option<i32>>,
     axis2: impl Into<Option<i32>>,
     stream: impl AsRef<Stream>,
-) -> Array {
+) -> Result<Array, Exception> {
     a.diagonal_device(offset, axis1, axis2, stream)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        array,
+        ops::{arange, diag, reshape},
+        Array,
+    };
+
+    use super::diagonal;
+
+    #[test]
+    fn test_diagonal() {
+        let x = Array::from_slice(&[0, 1, 2, 3, 4, 5, 6, 7], &[4, 2]);
+        let out = diagonal(&x, None, None, None).unwrap();
+        assert_eq!(out, array![0, 3]);
+
+        assert!(diagonal(&x, 1, 6, 0).is_err());
+        assert!(diagonal(&x, 1, 0, -3).is_err());
+
+        let x = Array::from_slice(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], &[3, 4]);
+        let out = diagonal(&x, 2, 1, 0).unwrap();
+        assert_eq!(out, array![8]);
+
+        let out = diagonal(&x, -1, 0, 1).unwrap();
+        assert_eq!(out, array![4, 9]);
+
+        let mut out = diagonal(&x, -5, 0, 1).unwrap();
+        out.eval().unwrap();
+        assert_eq!(out.shape(), &[0]);
+
+        let x = Array::from_slice(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], &[3, 2, 2]);
+        let out = diagonal(&x, 1, 0, 1).unwrap();
+        assert_eq!(out, array![[2], [3]]);
+
+        let out = diagonal(&x, 0, 2, 0).unwrap();
+        assert_eq!(out, array![[0, 5], [2, 7]]);
+
+        let out = diagonal(&x, 1, -1, 0).unwrap();
+        assert_eq!(out, array![[4, 9], [6, 11]]);
+
+        let x = reshape(&arange::<f32, _>(None, 16, None).unwrap(), &[2, 2, 2, 2]).unwrap();
+        let out = diagonal(&x, 0, 0, 1).unwrap();
+        assert_eq!(out, Array::from_slice(&[0, 12, 1, 13, 2, 14, 3, 15], &[2, 2, 2]));
+
+        assert!(diagonal(&x, 0, 1, 1).is_err());
+
+        let x = array![0, 1];
+        assert!(diagonal(&x, 0, 0, 1).is_err());
+    }
+
+    #[test]
+    fn test_diag() {
+        // Too few or too many dimensions
+        assert!(diag(&Array::from_float(0.0), None).is_err());
+        assert!(diag(&Array::from_slice(&[0.0], &[1, 1, 1]), None).is_err());
+
+        // Test with 1D array
+        let x = array![0, 1, 2, 3];
+        let out = diag(&x, 0).unwrap();
+        assert_eq!(
+            out,
+            array![
+                [0, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 2, 0],
+                [0, 0, 0, 3]
+            ]
+        );
+
+        let out = diag(&x, 1).unwrap();
+        assert_eq!(
+            out,
+            array![
+                [0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 2, 0],
+                [0, 0, 0, 0, 3],
+                [0, 0, 0, 0, 0]
+            ]
+        );
+
+        let out = diag(&x, -1).unwrap();
+        assert_eq!(
+            out,
+            array![
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0],
+                [0, 0, 2, 0, 0],
+                [0, 0, 0, 3, 0]
+            ]
+        );
+
+        // Test with 2D array
+        let x = Array::from_slice(&[0, 1, 2, 3, 4, 5, 6, 7, 8], &[3, 3]);
+        let out = diag(&x, 0).unwrap();
+        assert_eq!(out, array![0, 4, 8]);
+
+        let out = diag(&x, 1).unwrap();
+        assert_eq!(out, array![1, 5]);
+
+        let out = diag(&x, -1).unwrap();
+        assert_eq!(out, array![3, 7]);
+    }
 }

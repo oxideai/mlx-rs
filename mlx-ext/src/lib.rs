326f0d853d4232298338d68da140cd6b329a03d6
minghuaw <michael.wu1107@gmail.com>
2024-07-29 11:30:50 -0700

    copied arith ops

--- mlx-ext/src/lib.rs
+++ mlx-ext/src/lib.rs
@@ -1,1 +1,8 @@
 //! Extension crate for the `mlx` with a focus on improving the ergonomics.
+
+// We are re-exporting here so that we can refer to `mlx_rs` in the macros even if the user
+// doesn't have it in their dependencies or renamed it.
+pub use mlx_rs;
+
+#[cfg(feature = "macros")]
+mod macros;

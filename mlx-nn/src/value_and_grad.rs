a0847b4bede5db4b1b5d97a9301575bcc63576c7
minghuaw <michael.wu1107@gmail.com>
2024-09-07 17:43:45 -0700

    move closure inner into the returned closure

--- mlx-nn/src/value_and_grad.rs
+++ mlx-nn/src/value_and_grad.rs
@@ -8,29 +8,27 @@ use crate::module::{FlattenedModuleParam, FlattenedModuleParamRef, Module};
 /// TODO: a better name? swift binding uses just `value_and_grad` but the base crate `mlx-rs` also
 /// has one
 pub fn value_and_grad<'a, M, F, Args>(
-    model: &'a M,
     mut f: F,
-) -> impl FnMut(Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a
+) -> impl FnMut(&'a M, Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a
 where
     M: Module + 'a,
     F: FnMut(&M, Args) -> Vec<Array> + 'a,
     Args: Clone,
 {
-    // We need to have the parameters in the closure so that the gradient will be computed
-    // wrt them
-    let inner = move |(_parameters, arrays): (FlattenedModuleParamRef, Args)| -> Vec<Array> {
-        // We either have to clone here or clone inside value_and_grad
-        // let flattened_parameters = parameters.into_iter().map(|(k, v)| (k, v.clone()));
-
-        // This is needed in the swift binding, otherwise the gradients will all be zero
-        // However, we might not need this
-        // update_flattened_parameters(model, flattened_parameters);
-        f(model, arrays)
-    };
-
-    let mut vg = mlx_rs::transforms::value_and_grad_with_hashmap(inner);
-
-    move |arrays| {
+    move |model, arrays| {
+        // We need to have the parameters in the closure so that the gradient will be computed
+        // wrt them
+        let inner = |(_parameters, arrays): (FlattenedModuleParamRef, Args)| -> Vec<Array> {
+            // We either have to clone here or clone inside value_and_grad
+            // let flattened_parameters = parameters.into_iter().map(|(k, v)| (k, v.clone()));
+            
+            // This is needed in the swift binding, otherwise the gradients will all be zero
+            // However, we might not need this
+            // update_flattened_parameters(model, flattened_parameters);
+            f(model, arrays)
+        };
+        let mut vg = mlx_rs::transforms::value_and_grad_with_hashmap(inner);
+        
         let trainable_parameters = model.trainable_parameters().flatten();
         let (v, g) = vg((trainable_parameters, arrays))?;
         Ok((v, g))

22d44bb115d95f44c453f999432a2282ca1560b8
minghuaw <michael.wu1107@gmail.com>
2024-09-09 01:33:12 -0700

    add example in ModuleParameters doc

--- mlx-macros/src/lib.rs
+++ mlx-macros/src/lib.rs
@@ -145,6 +145,48 @@ pub fn generate_test_cases(input: TokenStream) -> TokenStream {
     TokenStream::from(tests)
 }
 
+/// Derive the `ModuleParameters` trait for a struct. Mark a field with `#[param]` attribute to
+/// include it in the parameters. The field type must implement the `Parameter` trait defined in
+/// `mlx-nn-module` crate.
+/// 
+/// Make sure to include `mlx-nn-module` as a dependency in your `Cargo.toml`.
+/// 
+/// # Example
+/// 
+/// ```rust, ignore
+/// use mlx_macros::ModuleParameters;
+/// use mlx_nn_module::{ModuleParameters, Param};
+/// 
+/// #[derive(ModuleParameters)]
+/// struct Example {
+///     #[param]
+///     regular: Param<Array>,
+/// 
+///     #[param]
+///     optional: Param<Option<Array>>,
+/// 
+///     #[param]
+///     nested: Param<Inner>,
+/// 
+///     #[param]
+///     vec: Param<Vec<Array>>,
+/// 
+///     #[param]
+///     vec_nested: Param<Vec<Inner>>,
+/// 
+///     #[param]
+///     trait_object: Param<Box<dyn Module>>,
+/// 
+///     #[param]
+///     trait_object_vec: Param<Vec<Box<dyn Module>>>,
+/// }
+/// 
+/// #[derive(ModuleParameters)]
+/// struct Inner {
+///     #[param]
+///     a: Param<Array>,
+/// }
+/// ```
 #[proc_macro_derive(ModuleParameters, attributes(param))]
 pub fn derive_module_parameters(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);

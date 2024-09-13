c7a21d5b03f44e6c7286d89dce4bf07d0602cf3a
minghuaw <michael.wu1107@gmail.com>
2024-09-13 00:49:31 -0700

    added unit test for Sgd

--- mlx-nn/src/optimizer/sgd.rs
+++ mlx-nn/src/optimizer/sgd.rs
@@ -7,6 +7,48 @@ use crate::utils::get_mut_or_insert_with;
 
 use super::Optimizer;
 
+/// Optional configs for the SGD optimizer.
+#[derive(Debug, Clone, Default)]
+pub struct SgdOptions {
+    /// Momentum strength
+    pub momentum: Option<f32>,
+
+    /// Weight decay (L2 penalty)
+    pub weight_decay: Option<f32>,
+
+    /// Dampening for momentum
+    pub dampening: Option<f32>,
+
+    /// Enables nesterov momentum
+    pub nesterov: Option<bool>,
+}
+
+impl SgdOptions {
+    /// Sets the momentum
+    pub fn with_momentum(mut self, momentum: impl Into<Option<f32>>) -> Self {
+        self.momentum = momentum.into();
+        self
+    }
+
+    /// Sets the weight decay
+    pub fn with_weight_decay(mut self, weight_decay: impl Into<Option<f32>>) -> Self {
+        self.weight_decay = weight_decay.into();
+        self
+    }
+
+    /// Sets the dampening for momentum
+    pub fn with_dampening(mut self, dampening: impl Into<Option<f32>>) -> Self {
+        self.dampening = dampening.into();
+        self
+    }
+
+    /// Enables nesterov momentum
+    pub fn with_nesterov(mut self, nesterov: impl Into<Option<bool>>) -> Self {
+        self.nesterov = nesterov.into();
+        self
+    }
+}
+
 /// Stochastic gradient descent optimizer.
 #[derive(Debug, Clone)]
 pub struct Sgd {
@@ -27,22 +69,6 @@ pub struct Sgd {
 
     /// Inner state
     pub state: FlattenedModuleParam,
-}
-
-/// Optional configs for the SGD optimizer.
-#[derive(Debug, Clone)]
-pub struct SgdOptions {
-    /// Momentum strength
-    pub momentum: Option<f32>,
-
-    /// Weight decay (L2 penalty)
-    pub weight_decay: Option<f32>,
-
-    /// Dampening for momentum
-    pub dampening: Option<f32>,
-
-    /// Enables nesterov momentum
-    pub nesterov: Option<bool>,
 }
 
 impl Sgd {
@@ -111,5 +137,73 @@ impl Optimizer for Sgd {
                 self.update_single(key, gradient, parameter);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use mlx_macros::ModuleParameters;
+    use mlx_nn_module::{ModuleParameters, Param};
+    use mlx_rs::ops::{ones, zeros};
+
+    use super::*;
+
+    #[derive(Debug, ModuleParameters)]
+    struct First {
+        #[param]
+        a: Param<Array>,
+
+        #[param]
+        b: Param<Array>,
+    }
+
+    #[derive(Debug, ModuleParameters)]
+    struct Model {
+        #[param]
+        first: Param<First>,
+
+        #[param]
+        second: Param<Array>,
+    }
+
+    #[test]
+    fn test_sgd() {
+        let first = First {
+            a: Param::new(zeros::<f32>(&[10]).unwrap()),
+            b: Param::new(zeros::<f32>(&[1]).unwrap()),
+        };
+        let mut model = Model {
+            first: Param::new(first),
+            second: Param::new(zeros::<f32>(&[1]).unwrap()),
+        };
+        // let param_map = param.parameters_mut().flatten();
+        let grads_map: FlattenedModuleParam = model
+            .parameters()
+            .flatten()
+            .iter()
+            .map(|(k, v)| {
+                let g = ones::<f32>(v.shape()).unwrap();
+                (k.clone(), g)
+            })
+            .collect();
+
+        let mut optim = Sgd::new(1e-2, SgdOptions::default().with_momentum(0.9));
+        optim.update(&mut model, grads_map);
+
+        let expected_first_a = ones::<f32>(&[10]).unwrap() * array!(-0.01);
+        let expected_first_b = ones::<f32>(&[1]).unwrap() * array!(-0.01);
+        let expected_second = ones::<f32>(&[1]).unwrap() * array!(-0.01);
+        assert_eq!(model.first.a.as_ref(), &expected_first_a);
+        assert_eq!(model.first.b.as_ref(), &expected_first_b);
+        assert_eq!(model.second.as_ref(), &expected_second);
+
+        let expected_state_first_a = ones::<f32>(&[10]).unwrap();
+        let expected_state_first_b = ones::<f32>(&[1]).unwrap();
+        let expected_state_second = ones::<f32>(&[1]).unwrap();
+
+        assert_eq!(optim.state.get("first.a"), Some(&expected_state_first_a));
+        assert_eq!(optim.state.get("first.b"), Some(&expected_state_first_b));
+        assert_eq!(optim.state.get("second"), Some(&expected_state_second));
     }
 }

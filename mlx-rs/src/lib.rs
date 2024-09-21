1ef74611ab3a7a0f86b9d1602cedb7abf4881559
minghuaw <michael.wu1107@gmail.com>
2024-09-21 01:04:28 -0700

    Restrict use of ScalarOrArray trait to operator impl and ClipBound (#108)
    * restrict use of ScalarOrArray trait to operator impl and ClipBound
    
    * fix doctest

--- mlx-rs/src/lib.rs
+++ mlx-rs/src/lib.rs
@@ -28,7 +28,6 @@ pub mod prelude {
         dtype::Dtype,
         ops::indexing::{Ellipsis, IndexMutOp, IndexOp, IntoStrideBy, NewAxis},
         stream::StreamOrDevice,
-        utils::ScalarOrArray,
     };
 
     pub use num_traits::Pow;

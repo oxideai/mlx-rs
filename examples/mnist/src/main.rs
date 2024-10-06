32d32931e6e941a441a458d32e00937371a67f6d
minghuaw <michael.wu1107@gmail.com>
2024-10-06 09:25:23 -0700

    initial nn example

--- examples/mnist/src/main.rs
+++ examples/mnist/src/main.rs
@@ -1,9 +1,63 @@
+use mlx_nn::{losses::{CrossEntropyOptions, LossReduction}, module_value_and_grad, optimizers::Optimizer};
+use mlx_rs::{array, error::Exception, module::{Module, ModuleParameters}, transforms::eval, Array};
+
 /// MLP model
 mod mlp;
 
 /// Retrieves MNIST dataset
 mod mnist;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Clone)]
+struct Loader { }
+
+impl Iterator for Loader {
+    type Item = (Array, Array);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
+fn load_training_data() -> Result<Loader, Box<dyn std::error::Error>> {
+    todo!()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let num_layers = 3;
+    let input_dim = 784;
+    let hidden_dim = 256;
+    let output_dim = 10;
+    let lr = 1e-2;
+    let num_epochs = 10;
+
+    let loader = load_training_data()?;
+    let mut model = mlp::Mlp::new(num_layers, input_dim, hidden_dim, output_dim)?;
+
+    let loss_fn = |model: &mlp::Mlp, (x, y): (&Array, &Array)| -> Result<Array, Exception> {
+        let y_pred = model.forward(x)?;
+
+        // Config optional parameters for cross entropy loss
+        let options = CrossEntropyOptions::builder()
+            .reduction(LossReduction::Mean)
+            .build();
+        mlx_nn::losses::cross_entropy(y_pred, y, options)
+    };
+    let mut loss_and_grad_fn = module_value_and_grad(loss_fn);
+    
+    let mut optimizer = mlx_nn::optimizers::Sgd::new(lr);
+
+    for _ in 0..num_epochs {
+        let mut loss_sum = array!(0.0);
+        for (x, y) in loader.clone() {
+            let (loss, grad) = loss_and_grad_fn(&mut model, (&x, &y))?;
+            optimizer.update(&mut model, grad);
+            eval(model.parameters().flatten().values().map(|v| *v))?;
+
+            loss_sum += loss;
+        }
+
+        println!("Epoch: {}, Loss sum: {}", num_epochs, loss_sum);
+    }
+
+    Ok(())
 }
--- examples/mnist/src/mlp.rs
+++ examples/mnist/src/mlp.rs
@@ -1,7 +1,44 @@
-use mlx_nn::{macros::ModuleParameters, module::Param, Linear};
+use mlx_nn::{macros::ModuleParameters, module::Param, Linear, Relu, Sequential};
+use mlx_rs::{error::Exception, module::Module, Array};
 
 #[derive(Debug, ModuleParameters)]
 pub struct Mlp {
     #[param]
-    layers: Param<Vec<Linear>>
+    pub layers: Param<Sequential>,
+}
+
+impl Module for Mlp {
+    type Error = Exception;
+
+    fn forward(&self, x: &Array) -> Result<Array, Self::Error> {
+        self.layers.forward(x)
+    }
+
+    fn training_mode(&mut self, mode: bool) {
+        self.layers.training_mode(mode);
+    }
+}
+
+impl Mlp {
+    pub fn new(num_layers: usize, input_dim: i32, hidden_dim: i32, output_dim: i32) -> Result<Self, Exception> {
+        let mut layers = Sequential::new();
+
+        // Add the input layer
+        layers = layers
+            .append(Linear::new(input_dim, hidden_dim)?)
+            .append(Relu::new());
+
+        // Add the hidden layers
+        for _ in 1..num_layers {
+            layers = layers.append(Linear::new(hidden_dim, hidden_dim)?)
+                .append(Relu::new());
+        }
+
+        // Add the output layer
+        layers = layers.append(Linear::new(hidden_dim, output_dim)?);
+
+        Ok(Self {
+            layers: Param::new(layers),
+        })
+    }
 }

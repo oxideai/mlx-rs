b808c9fcdea80c7391f6333805bb5cc9bd878de4
minghuaw <michael.wu1107@gmail.com>
2024-10-06 09:25:04 -0700

    fix lifetime issue

--- mlx-nn/src/value_and_grad.rs
+++ mlx-nn/src/value_and_grad.rs
@@ -3,8 +3,17 @@ use mlx_rs::{error::Exception, Array};
 
 use crate::module::{FlattenedModuleParam, FlattenedModuleParamRef, Module};
 
+fn trainable_params(model: &impl Module) -> FlattenedModuleParam {
+    model
+        .trainable_parameters()
+        .flatten()
+        .into_iter()
+        .map(|(k, v)| (k, v.clone()))
+        .collect()
+}
+
 /// Helper trait for [`value_and_grad`]
-pub trait IntoModuleValueAndGrad<'a, M, Args, Err>
+pub trait IntoModuleValueAndGrad<'a, M, Args, Val, Err>
 where
     M: Module + 'a,
     Args: Clone,
@@ -13,43 +22,10 @@ where
     /// model's trainable parameters.
     fn into_module_value_and_grad(
         self,
-    ) -> impl FnMut(&'a mut M, Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a;
+    ) -> impl FnMut(&mut M, Args) -> Result<(Val, FlattenedModuleParam), Exception> + 'a;
 }
 
-macro_rules! into_module_value_and_grad_inner {
-    ($inner_ret_ty:ty, $f:ident) => {
-        move |model, arrays| {
-            // We have to clone here to avoid issue with the mutable borrow of `model` in the closure
-            let trainable_parameters = model
-                .trainable_parameters()
-                .flatten()
-                .into_iter()
-                .map(|(k, v)| (k, v.clone()))
-                .collect();
-
-            // We need to have the parameters in the closure so that the gradient will be computed wrt
-            // them
-            let inner = |parameters: FlattenedModuleParamRef, arrays: Args| -> $inner_ret_ty {
-                // Somehow the parameters of the model captured in the closure are not the same arrays
-                // as the ones passed in the outer function (their memory address are actually different
-                // in the swift binding).
-                //
-                // We need to update the parameters of the model with the ones passed in, otherwise the
-                // gradients will be zero
-                let flattened_parameters = parameters.into_iter().map(|(k, v)| (k, v.clone()));
-                update_flattened_parameters(model, flattened_parameters);
-
-                ($f)(model, arrays)
-            };
-            let mut vg = mlx_rs::transforms::value_and_grad_with_hashmap(inner);
-
-            let (v, g) = vg(trainable_parameters, arrays)?;
-            Ok((v, g))
-        }
-    };
-}
-
-impl<'a, F, M, Args> IntoModuleValueAndGrad<'a, M, Args, ()> for F
+impl<'a, F, M, Args> IntoModuleValueAndGrad<'a, M, Args, Vec<Array>, ()> for F
 where
     M: Module + 'a,
     F: FnMut(&M, Args) -> Vec<Array> + 'a,
@@ -57,13 +33,25 @@ where
 {
     fn into_module_value_and_grad(
         mut self,
-    ) -> impl FnMut(&'a mut M, Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a
+    ) -> impl FnMut(&mut M, Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a
     {
-        into_module_value_and_grad_inner!(Vec<Array>, self)
+        move |model, arrays| {
+            let trainable_parameters = trainable_params(model);
+            let inner = |parameters: FlattenedModuleParamRef, arrays: Args| -> Vec<Array> {
+                let flattened_parameters = parameters.into_iter().map(|(k, v)| (k, v.clone()));
+                update_flattened_parameters(model, flattened_parameters);
+
+                self(model, arrays)
+            };
+            let mut vg = mlx_rs::transforms::value_and_grad_with_hashmap(inner);
+
+            let (v, g) = vg(trainable_parameters, arrays)?;
+            Ok((v, g))
+        }
     }
 }
 
-impl<'a, F, M, Args> IntoModuleValueAndGrad<'a, M, Args, Exception> for F
+impl<'a, F, M, Args> IntoModuleValueAndGrad<'a, M, Args, Vec<Array>, Exception> for F
 where
     M: Module + 'a,
     F: FnMut(&M, Args) -> Result<Vec<Array>, Exception> + 'a,
@@ -71,20 +59,86 @@ where
 {
     fn into_module_value_and_grad(
         mut self,
-    ) -> impl FnMut(&'a mut M, Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a
+    ) -> impl FnMut(&mut M, Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a
     {
-        into_module_value_and_grad_inner!(Result<Vec<Array>, Exception>, self)
+        move |model, arrays| {
+            let trainable_parameters = trainable_params(model);
+            let inner = |parameters: FlattenedModuleParamRef, arrays: Args| -> Result<Vec<Array>, Exception> {
+                let flattened_parameters = parameters.into_iter().map(|(k, v)| (k, v.clone()));
+                update_flattened_parameters(model, flattened_parameters);
+
+                self(model, arrays)
+            };
+            let mut vg = mlx_rs::transforms::value_and_grad_with_hashmap(inner);
+
+            let (v, g) = vg(trainable_parameters, arrays)?;
+            Ok((v, g))
+        }
+    }
+}
+
+impl<'a, F, M, Args> IntoModuleValueAndGrad<'a, M, Args, Array, ()> for F
+where 
+    M: Module + 'a,
+    F: FnMut(&M, Args) -> Array + 'a,
+    Args: Clone,
+{
+    fn into_module_value_and_grad(
+        mut self,
+    ) -> impl FnMut(&mut M, Args) -> Result<(Array, FlattenedModuleParam), Exception> + 'a
+    {
+        move |model, arrays| {
+            let trainable_parameters = trainable_params(model);
+            let inner = |parameters: FlattenedModuleParamRef, arrays: Args| -> Vec<Array> {
+                let flattened_parameters = parameters.into_iter().map(|(k, v)| (k, v.clone()));
+                update_flattened_parameters(model, flattened_parameters);
+
+                vec![self(model, arrays)]
+            };
+            let mut vg = mlx_rs::transforms::value_and_grad_with_hashmap(inner);
+
+            let (v, g) = vg(trainable_parameters, arrays)?;
+            let v = v.into_iter().next().expect("Expected a single value");
+            Ok((v, g))
+        }
+    }
+}
+
+impl<'a, F, M, Args> IntoModuleValueAndGrad<'a, M, Args, Array, Exception> for F
+where 
+    M: Module + 'a,
+    F: FnMut(&M, Args) -> Result<Array, Exception> + 'a,
+    Args: Clone,
+{
+    fn into_module_value_and_grad(
+        mut self,
+    ) -> impl FnMut(&mut M, Args) -> Result<(Array, FlattenedModuleParam), Exception> + 'a
+    {
+        move |model, arrays| {
+            let trainable_parameters = trainable_params(model);
+            let inner = |parameters: FlattenedModuleParamRef, arrays: Args| -> Result<Vec<Array>, Exception> {
+                let flattened_parameters = parameters.into_iter().map(|(k, v)| (k, v.clone()));
+                update_flattened_parameters(model, flattened_parameters);
+
+                self(model, arrays).map(|v| vec![v])
+            };
+            let mut vg = mlx_rs::transforms::value_and_grad_with_hashmap(inner);
+
+            let (v, g) = vg(trainable_parameters, arrays)?;
+            let v = v.into_iter().next().expect("Expected a single value");
+            Ok((v, g))
+        }
     }
 }
 
 /// Transform the passed function `f(model, args)` to a function that computes the gradients of `f`
 /// with regard to the model's trainable parameters and also its value.
-pub fn module_value_and_grad<'a, F, M, Args, Err>(
+pub fn module_value_and_grad<'a, F, M, Args, Val, Err>(
     f: F,
-) -> impl FnMut(&'a mut M, Args) -> Result<(Vec<Array>, FlattenedModuleParam), Exception> + 'a
+) -> impl FnMut(&mut M, Args) -> Result<(Val, FlattenedModuleParam), Exception> + 'a
 where
     M: Module + 'a,
-    F: IntoModuleValueAndGrad<'a, M, Args, Err>,
+    F: IntoModuleValueAndGrad<'a, M, Args, Val, Err>,
     Args: Clone,
 {
     f.into_module_value_and_grad()
@@ -129,6 +183,24 @@ mod tests {
 
         let mut vg = module_value_and_grad(loss);
         let (v, g) = vg(&mut model, &x).unwrap();
+
+        assert_ne!(v[0].sum(None, None).unwrap(), array!(0.0));
+        assert_ne!(g["weight"].sum(None, None).unwrap(), array!(0.0));
+        assert_ne!(g["bias"].sum(None, None).unwrap(), array!(0.0));
+    }
+
+    #[test]
+    fn test_module_value_and_grad_with_two_args() {
+        let mut model = Linear::new(2, 2).unwrap();
+        let x = mlx_rs::random::uniform::<_, f32>(1.0, 2.0, &[2, 2], None).unwrap();
+        let y = mlx_rs::ops::ones::<f32>(x.shape()).unwrap();
+
+        let loss = |model: &Linear, (x, y): (&Array, &Array)| -> Result<Vec<Array>, Exception> {
+            model.forward(x)?.subtract(y)?.square().sum(None, None).map(|v| vec![v])
+        };
+
+        let mut vg = module_value_and_grad(loss);
+        let (v, g) = vg(&mut model, (&x, &y)).unwrap();
 
         assert_ne!(v[0].sum(None, None).unwrap(), array!(0.0));
         assert_ne!(g["weight"].sum(None, None).unwrap(), array!(0.0));

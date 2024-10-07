c04ce8857b3be313075308c22e96f1899241cad1
minghuaw <michael.wu1107@gmail.com>
2024-10-06 23:38:41 -0700

    added smoke tests

--- mlx-nn/src/optimizers/mod.rs
+++ mlx-nn/src/optimizers/mod.rs
@@ -88,3 +88,133 @@ mod optim_test_util {
 
     pub(super) const ATOL: f64 = 1e-5;
 }
+
+#[cfg(test)]
+mod tests {
+    use mlx_macros::ModuleParameters;
+    use mlx_rs::{
+        array,
+        error::Exception,
+        module::{Module, ModuleParameters, Param},
+        random::uniform,
+        transforms::{eval, eval_params},
+        Array,
+    };
+
+    use crate::{
+        losses::{mse_loss, LossReduction, MseLossOptions},
+        module_value_and_grad,
+    };
+
+    use super::*;
+
+    /// A helper model for testing optimizers.
+    ///
+    /// This is adapted from the swift binding tests in `mlx-swift/Tests/MLXTests/OptimizerTests.swift`.
+    #[derive(Debug, ModuleParameters)]
+    struct LinearFunctionModel {
+        #[param]
+        pub m: Param<Array>,
+
+        #[param]
+        pub b: Param<Array>,
+    }
+
+    impl Module for LinearFunctionModel {
+        type Error = Exception;
+
+        fn forward(&self, x: &Array) -> Result<Array, Self::Error> {
+            self.m.multiply(x)?.add(&self.b)
+        }
+
+        fn training_mode(&mut self, _mode: bool) {}
+    }
+
+    impl LinearFunctionModel {
+        pub fn new() -> Result<Self, Exception> {
+            let m = uniform::<_, f32>(-5.0, 5.0, None, None)?;
+            let b = uniform::<_, f32>(-5.0, 5.0, None, None)?;
+            Ok(Self {
+                m: Param::new(m),
+                b: Param::new(b),
+            })
+        }
+    }
+
+    pub fn train<F, O>(f: F, steps: usize) -> Result<Array, Box<dyn std::error::Error>>
+    where
+        F: FnOnce() -> O,
+        O: Optimizer,
+    {
+        let mut optimizer = f();
+
+        let options = MseLossOptions::builder()
+            .reduction(LossReduction::Mean)
+            .build();
+        let loss = |model: &LinearFunctionModel, (x, y): (&Array, &Array)| {
+            mse_loss(model.forward(x)?, y, &options)
+        };
+
+        // TODO: check compiled model once we have it
+        let mut model = LinearFunctionModel::new()?;
+        eval_params(model.parameters())?;
+
+        let m = array!(0.25);
+        let b = array!(7.0);
+
+        let mut lg = module_value_and_grad(loss);
+
+        let mut last_loss = None;
+        for _ in 0..steps {
+            // println!("target: b = {}, m = {}", b, m);
+            // println!("parameters: {:?}", model.parameters());
+
+            // generate random training data along with the ground truth.
+            // notice that the shape is [B, 1] where B is the batch
+            // dimension -- this allows us to train on 10 samples simultaneously
+            let x = uniform::<_, f32>(-5.0, 5.0, &[10, 1], None)?;
+            let y = &m * &x + &b;
+            eval([&x, &y])?;
+
+            // compute the loss and gradients.  use the optimizer
+            // to adjust the parameters closer to the target
+            let (loss, g) = lg(&mut model, (&x, &y))?;
+            optimizer.update(&mut model, g);
+
+            eval_params(model.parameters())?;
+
+            last_loss = Some(loss);
+        }
+
+        Ok(last_loss.unwrap())
+    }
+
+    const NUM_TRIALS: usize = 3;
+
+    #[test]
+    fn test_sgd_converges() {
+        let mut total_loss = 0.0;
+        for _ in 0..NUM_TRIALS {
+            let loss = train(|| Sgd::new(0.1), 30).unwrap();
+            total_loss += loss.item::<f32>();
+        }
+        // It sometimes doesn't converge that fast, so we take the average loss
+        // across multiple trials
+        let avg_loss = total_loss / NUM_TRIALS as f32;
+        assert!(avg_loss < 0.1, "avg loss: {}", avg_loss);
+    }
+
+    #[test]
+    fn test_rmsprop_converges() {
+        let mut total_loss = 0.0;
+        for _ in 0..NUM_TRIALS {
+            // RMSProp doesn't seem to converge as fast as SGD
+            let loss = train(|| RmsProp::new(0.1), 100).unwrap();
+            total_loss += loss.item::<f32>();
+        }
+        // It sometimes doesn't converge that fast, so we take the average loss
+        // across multiple trials
+        let avg_loss = total_loss / NUM_TRIALS as f32;
+        assert!(avg_loss < 0.1, "avg loss: {}", avg_loss);
+    }
+}

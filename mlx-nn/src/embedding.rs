51a906a12c482a94d1c24cf3ee9aa8faaceae816
David Chavez <david@dcvz.io>
2024-10-28 10:44:46 +0100

    Address PR comments

--- mlx-nn/src/embedding.rs
+++ mlx-nn/src/embedding.rs
@@ -48,7 +48,7 @@ impl Module for Embedding {
     type Error = Exception;
 
     fn forward(&self, x: &Array) -> Result<Array, Self::Error> {
-        Ok(self.weight.index(x.clone()))
+        Ok(self.weight.index(x))
     }
 
     fn training_mode(&mut self, _mode: bool) {}
@@ -56,15 +56,23 @@ impl Module for Embedding {
 
 #[cfg(test)]
 mod tests {
+    use float_eq::float_eq;
+    use pretty_assertions::assert_eq;
     use super::*;
-    use mlx_rs::array;
 
     #[test]
     fn test_embedding() {
-        let embedding = Embedding::new(10, 3).unwrap();
-        let input = array!([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-        let output = embedding.forward(&input).unwrap();
+        mlx_rs::random::seed(557);
+        let a = mlx_rs::random::randint::<_, i32>(0, 10, &[2, 8, 8, 4], None).unwrap();
+        assert_eq!(a.shape(), &[2, 8, 8, 4]);
+        assert_eq!(a.dtype(), mlx_rs::Dtype::Int32);
+        float_eq!(a.mean(None, None).unwrap().item::<f32>(), 4.60546875, abs <= 0.09210937500000001);
+        float_eq!(a.sum(None, None).unwrap().item::<f32>(), 2358.0, abs <= 47.160000000000004);
 
-        assert_eq!(output.shape(), [10, 3]);
+        let result = Embedding::new(10, 8).unwrap().forward(&a).unwrap();
+        assert_eq!(result.shape(), &[2, 8, 8, 4, 8]);
+        assert_eq!(result.dtype(), mlx_rs::Dtype::Float32);
+        float_eq!(result.mean(None, None).unwrap().item::<f32>(), -0.0011973462533205748, abs <= 2.3946925066411497e-05);
+        float_eq!(result.sum(None, None).unwrap().item::<f32>(), -4.904330253601074, abs <= 0.0980866050720215);
     }
 }
--- mlx-nn/src/lib.rs
+++ mlx-nn/src/lib.rs
@@ -15,7 +15,7 @@ mod activation;
 mod container;
 mod convolution;
 mod dropout;
-pub mod embedding;
+mod embedding;
 mod linear;
 mod value_and_grad;
 

0683178be2665c79fb2a913a28ddd705d7d5d30a
minghuaw <michael.wu1107@gmail.com>
2024-12-01 15:14:33 -0800

    impl TransformerDecoderLayer

--- mlx-nn/src/transformer.rs
+++ mlx-nn/src/transformer.rs
@@ -398,6 +398,7 @@ where
         self.linear2.training_mode(mode);
         self.dropout1.training_mode(mode);
         self.dropout2.training_mode(mode);
+        self.activation.training_mode(mode);
     }
 }
 
@@ -486,5 +487,184 @@ where
             <TransformerEncoderLayer as Module<TransformerEncoderInput>>::training_mode(layer, mode);
         });
         self.ln.training_mode(mode);
+    }
+}
+
+#[derive(Debug, Builder)]
+#[builder(
+    build_with = build_transformer_decoder_layer,
+    err = TransformerBulidError,
+)]
+struct TransformerDecoderLayerBuilder {
+    pub dimensions: i32,
+    pub num_heads: i32,
+    #[builder(optional, default = None)]
+    pub ml_dimensions: Option<i32>,
+    #[builder(optional, default = Self::DEFAULT_DROPOUT)]
+    pub dropout: f32,
+    #[builder(optional, default = None)]
+    pub activation: Option<Box<dyn Activation>>,
+    pub norm_first: bool,
+}
+
+impl TransformerDecoderLayerBuilder {
+    const DEFAULT_DROPOUT: f32 = 0.0;
+}
+
+fn build_transformer_decoder_layer(builder: TransformerDecoderLayerBuilder) -> Result<TransformerDecoderLayer, TransformerBulidError> {
+    let dimensions = builder.dimensions;
+    let num_heads = builder.num_heads;
+    let mlp_dimensions = builder.ml_dimensions.unwrap_or(4 * dimensions);
+    let dropout = builder.dropout;
+
+    let self_attention = MultiHeadAttention::new(dimensions, num_heads)?;
+    let cross_attention = MultiHeadAttention::new(dimensions, num_heads)?;
+    let ln1 = LayerNorm::new(dimensions)?;
+    let ln2 = LayerNorm::new(dimensions)?;
+    let ln3 = LayerNorm::new(dimensions)?;
+    let linear1 = Linear::new(dimensions, mlp_dimensions)?;
+    let linear2 = Linear::new(mlp_dimensions, dimensions)?;
+    let dropout1 = DropoutBuilder::new().p(dropout).build()?;
+    let dropout2 = DropoutBuilder::new().p(dropout).build()?;
+    let dropout3 = DropoutBuilder::new().p(dropout).build()?;
+    let activation = builder.activation.unwrap_or(Box::new(Relu));
+    let norm_first = builder.norm_first;
+
+    Ok(TransformerDecoderLayer {
+        self_attention,
+        cross_attention,
+        ln1,
+        ln2,
+        ln3,
+        linear1,
+        linear2,
+        dropout1,
+        dropout2,
+        dropout3,
+        activation,
+        norm_first,
+    })
+}
+
+#[derive(Debug, ModuleParameters, Buildable)]
+struct TransformerDecoderLayer {
+    #[param] 
+    pub self_attention: MultiHeadAttention,
+    
+    #[param] 
+    pub cross_attention: MultiHeadAttention,
+    
+    #[param] 
+    pub ln1: LayerNorm,
+    
+    #[param] 
+    pub ln2: LayerNorm,
+    
+    #[param] 
+    pub ln3: LayerNorm,
+    
+    #[param] 
+    pub linear1: Linear,
+    
+    #[param] 
+    pub linear2: Linear,
+    
+    #[param] 
+    pub dropout1: Dropout,
+    
+    #[param] 
+    pub dropout2: Dropout,
+    
+    #[param] 
+    pub dropout3: Dropout,
+    
+    #[param] 
+    pub activation: Box<dyn Activation>,
+    
+    pub norm_first: bool,
+}
+
+struct TransformerDecoderInput<'a> {
+    pub x: &'a Array,
+    pub memory: &'a Array,
+    pub x_mask: &'a Array,
+    pub memory_mask: &'a Array,
+}
+
+impl<'a> From<(&'a Array, &'a Array, &'a Array, &'a Array)> for TransformerDecoderInput<'a> {
+    fn from((x, memory, x_mask, memory_mask): (&'a Array, &'a Array, &'a Array, &'a Array)) -> Self {
+        TransformerDecoderInput {
+            x,
+            memory,
+            x_mask,
+            memory_mask,
+        }
+    }
+}
+
+impl<'a, T> Module<T> for TransformerDecoderLayer 
+where 
+    T: Into<TransformerDecoderInput<'a>>,
+{
+    type Error = Exception;
+
+    type Output = Array;
+
+    fn forward(&mut self, input: T) -> Result<Self::Output, Self::Error> {
+        let input = input.into();
+        let x = input.x;
+        let memory = input.memory;
+        let x_mask = input.x_mask;
+        let memory_mask = input.memory_mask;
+
+        if self.norm_first {
+            let mut y = self.ln1.forward(x)?;
+            y = self.self_attention.forward((&y, &y, &y, x_mask))?;
+            y = self.dropout1.forward(&y)?;
+            let x = x.add(&y)?;
+
+            y = self.ln2.forward(&x)?;
+            y = self.cross_attention.forward((&y, memory, memory, memory_mask))?;
+            y = self.dropout2.forward(&y)?;
+            let x = x.add(&y)?;
+
+            y = self.ln3.forward(&x)?;
+            y = self.linear1.forward(&y)?;
+            y = self.activation.forward(&y)?;
+            y = self.dropout3.forward(&y)?;
+            y = self.linear2.forward(&y)?;
+            x.add(&y)
+        } else {
+            let mut y = self.self_attention.forward((x, x, x, x_mask))?;
+            y = self.dropout1.forward(&y)?;
+            let mut x = x.add(&y)?;
+            x = self.ln1.forward(&x)?;
+
+            y = self.cross_attention.forward((&y, memory, memory, memory_mask))?;
+            y = self.dropout2.forward(&y)?;
+            x = x.add(&y)?;
+            x = self.ln2.forward(&x)?; // TODO: https://github.com/ml-explore/mlx/issues/1636
+
+            y = self.linear1.forward(&x)?;
+            y = self.activation.forward(&y)?;
+            y = self.dropout3.forward(&y)?;
+            y = self.linear2.forward(&y)?;
+            y = x.add(&y)?;
+            self.ln3.forward(&y)
+        }
+    }
+
+    fn training_mode(&mut self, mode: bool) {
+        <MultiHeadAttention as Module<MultiHeadAttentionInput>>::training_mode(&mut self.self_attention, mode);
+        <MultiHeadAttention as Module<MultiHeadAttentionInput>>::training_mode(&mut self.cross_attention, mode);
+        self.ln1.training_mode(mode);
+        self.ln2.training_mode(mode);
+        self.ln3.training_mode(mode);
+        self.linear1.training_mode(mode);
+        self.linear2.training_mode(mode);
+        self.dropout1.training_mode(mode);
+        self.dropout2.training_mode(mode);
+        self.dropout3.training_mode(mode);
+        self.activation.training_mode(mode);
     }
 }

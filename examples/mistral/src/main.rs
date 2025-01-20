575f47a6ffc3ebfec23bfa681cad290279fc0104
minghuaw <michael.wu1107@gmail.com>
2025-01-19 21:15:14 -0800

    load tokenizer from hf

--- examples/mistral/src/main.rs
+++ examples/mistral/src/main.rs
@@ -1,4 +1,4 @@
-use hf_hub::{api::sync::Api, Repo};
+use hf_hub::{api::sync::{Api, ApiBuilder}, Repo};
 use tokenizers::Tokenizer;
 
 mod model;
@@ -7,8 +7,15 @@ type Error = Box<dyn std::error::Error + Send + Sync>;
 type Result<T, E = Error> = std::result::Result<T, E>;
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
-    let api = Api::new()?;
+    // Put your huggingface access token in a .env file located at the root of
+    // this example (ie. examples/mistral/.env)
+    dotenv::dotenv().ok();
+    let hf_token = std::env::var("HF_TOKEN")?;
+
+    let api = ApiBuilder::new()
+        .with_token(Some(hf_token))
+        .with_cache_dir("./data".into())
+        .build()?;
 
     let model_id = "mistralai/Mistral-7B-v0.1".to_string();
     let repo = api.repo(Repo::new(model_id, hf_hub::RepoType::Model));
@@ -16,7 +23,6 @@ fn main() -> Result<()> {
 
     let tokenizer = Tokenizer::from_file(tokenizer_filename)?;
     let vocab_size = tokenizer.get_vocab_size(false);
-    println!("Vocab size: {}", vocab_size);
 
     Ok(())
 }

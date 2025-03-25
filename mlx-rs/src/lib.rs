b3e2cbae69de14e79f7e459db38dc4c6584f1503
minghuaw <michael.wu1107@gmail.com>
2025-03-25 16:18:19 -0700

    add basics to docs

--- mlx-rs/src/lib.rs
+++ mlx-rs/src/lib.rs
@@ -1,5 +1,42 @@
 //! Unofficial rust bindings for the [MLX
 //! framework](https://github.com/ml-explore/mlx).
+//! 
+//! # Quick Start
+//! 
+//! ## Basics
+//! 
+//! ```rust
+//! use mlx_rs::{array, Dtype};
+//! 
+//! let a = array!([1, 2, 3, 4]);
+//! assert_eq!(a.shape(), &[4]);
+//! assert_eq!(a.dtype(), Dtype::Int32);
+//! 
+//! let b = array!([1.0, 2.0, 3.0, 4.0]);
+//! assert_eq!(b.dtype(), Dtype::Float32);
+//! ```
+//! 
+//! Operations in MLX are lazy. Use [`Array::eval`] to evaluate the
+//! the output of an operation. Operations are also automatically evaluated when
+//! inspecting an array with [`Array::item`], printing an array, or attempting to
+//! obtain the underlying data with [`Array::as_slice`].
+//! 
+//! ```rust
+//! use mlx_rs::{array, transforms::eval};
+//! 
+//! let a = array!([1, 2, 3, 4]);
+//! let b = array!([1.0, 2.0, 3.0, 4.0]);
+//! 
+//! let c = a + b; // c is not evaluated
+//! c.eval().unwrap(); // evaluates c
+//! 
+//! let d = a + b;
+//! println!("{:?}", d); // evaluates d
+//! 
+//! let e = a + b;
+//! let e_slice: &[f32] = e.as_slice().unwrap(); // evaluates e
+//! ```
+
 
 #![deny(unused_unsafe, missing_debug_implementations, missing_docs)]
 #![cfg_attr(test, allow(clippy::approx_constant))]

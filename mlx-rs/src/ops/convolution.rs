a47f2b7dac4077a33e07a57f6fc61f5c0d89f5c6
minghuaw <michael.wu1107@gmail.com>
2025-05-15 15:31:02 +0800

    updated conv and factory ops

--- mlx-rs/src/ops/convolution.rs
+++ mlx-rs/src/ops/convolution.rs
@@ -211,12 +211,14 @@ pub fn conv_transpose1d_device(
     #[optional] stride: impl Into<Option<i32>>,
     #[optional] padding: impl Into<Option<i32>>,
     #[optional] dilation: impl Into<Option<i32>>,
+    #[optional] output_padding: impl Into<Option<i32>>,
     #[optional] groups: impl Into<Option<i32>>,
     #[optional] stream: impl AsRef<Stream>,
 ) -> Result<Array> {
     let stride = stride.into().unwrap_or(1);
     let padding = padding.into().unwrap_or(0);
     let dilation = dilation.into().unwrap_or(1);
+    let output_padding = output_padding.into().unwrap_or(0);
     let groups = groups.into().unwrap_or(1);
 
     Array::try_from_op(|res| unsafe {
@@ -227,6 +229,7 @@ pub fn conv_transpose1d_device(
             stride,
             padding,
             dilation,
+            output_padding,
             groups,
             stream.as_ref().as_ptr(),
         )
@@ -255,12 +258,14 @@ pub fn conv_transpose2d_device(
     #[optional] stride: impl Into<Option<(i32, i32)>>,
     #[optional] padding: impl Into<Option<(i32, i32)>>,
     #[optional] dilation: impl Into<Option<(i32, i32)>>,
+    #[optional] output_padding: impl Into<Option<(i32, i32)>>,
     #[optional] groups: impl Into<Option<i32>>,
     #[optional] stream: impl AsRef<Stream>,
 ) -> Result<Array> {
     let stride = stride.into().unwrap_or((1, 1));
     let padding = padding.into().unwrap_or((0, 0));
     let dilation = dilation.into().unwrap_or((1, 1));
+    let output_padding = output_padding.into().unwrap_or((0, 0));
     let groups = groups.into().unwrap_or(1);
 
     Array::try_from_op(|res| unsafe {
@@ -274,6 +279,8 @@ pub fn conv_transpose2d_device(
             padding.1,
             dilation.0,
             dilation.1,
+            output_padding.0,
+            output_padding.1,
             groups,
             stream.as_ref().as_ptr(),
         )
@@ -302,12 +309,14 @@ pub fn conv_transpose3d_device(
     #[optional] stride: impl Into<Option<(i32, i32, i32)>>,
     #[optional] padding: impl Into<Option<(i32, i32, i32)>>,
     #[optional] dilation: impl Into<Option<(i32, i32, i32)>>,
+    #[optional] output_padding: impl Into<Option<(i32, i32, i32)>>,
     #[optional] groups: impl Into<Option<i32>>,
     #[optional] stream: impl AsRef<Stream>,
 ) -> Result<Array> {
     let stride = stride.into().unwrap_or((1, 1, 1));
     let padding = padding.into().unwrap_or((0, 0, 0));
     let dilation = dilation.into().unwrap_or((1, 1, 1));
+    let output_padding = output_padding.into().unwrap_or((0, 0, 0));
     let groups = groups.into().unwrap_or(1);
 
     Array::try_from_op(|res| unsafe {
@@ -324,6 +333,9 @@ pub fn conv_transpose3d_device(
             dilation.0,
             dilation.1,
             dilation.2,
+            output_padding.0,
+            output_padding.1,
+            output_padding.2,
             groups,
             stream.as_ref().as_ptr(),
         )
@@ -373,6 +385,7 @@ mod tests {
             Some(1), // stride
             Some(0), // padding
             Some(1), // dilation
+            None, // output padding
             Some(1), // groups
         )
         .unwrap();
@@ -423,6 +436,7 @@ mod tests {
             Some((1, 1)), // stride
             Some((0, 0)), // padding
             Some((1, 1)), // dilation
+            None, // output padding
             Some(1),      // groups
         )
         .unwrap();
@@ -481,6 +495,7 @@ mod tests {
             Some((1, 1, 1)), // stride
             Some((0, 0, 0)), // padding
             Some((1, 1, 1)), // dilation
+            None, // output padding
             Some(1),         // groups
         )
         .unwrap();
--- mlx-rs/src/ops/factory.rs
+++ mlx-rs/src/ops/factory.rs
@@ -242,14 +242,14 @@ impl Array {
     /// let r = Array::repeat_device::<i32>(source, 4, 1, StreamOrDevice::default()).unwrap();
     /// ```
     #[default_device]
-    pub fn repeat_device<T: ArrayElement>(
+    pub fn repeat_axis_device<T: ArrayElement>(
         array: Array,
         count: i32,
         axis: i32,
         stream: impl AsRef<Stream>,
     ) -> Result<Array> {
         Array::try_from_op(|res| unsafe {
-            mlx_sys::mlx_repeat(res, array.as_ptr(), count, axis, stream.as_ref().as_ptr())
+            mlx_sys::mlx_repeat_axis(res, array.as_ptr(), count, axis, stream.as_ref().as_ptr())
         })
     }
 
@@ -269,13 +269,13 @@ impl Array {
     /// let r = Array::repeat_all_device::<i32>(source, 4, StreamOrDevice::default()).unwrap();
     /// ```
     #[default_device]
-    pub fn repeat_all_device<T: ArrayElement>(
+    pub fn repeat_device<T: ArrayElement>(
         array: Array,
         count: i32,
         stream: impl AsRef<Stream>,
     ) -> Result<Array> {
         Array::try_from_op(|res| unsafe {
-            mlx_sys::mlx_repeat_all(res, array.as_ptr(), count, stream.as_ref().as_ptr())
+            mlx_sys::mlx_repeat(res, array.as_ptr(), count, stream.as_ref().as_ptr())
         })
     }
 
@@ -466,24 +466,24 @@ where
 /// See [`Array::repeat`]
 #[generate_macro]
 #[default_device]
-pub fn repeat_device<T: ArrayElement>(
+pub fn repeat_axis_device<T: ArrayElement>(
     array: Array,
     count: i32,
     axis: i32,
     #[optional] stream: impl AsRef<Stream>,
 ) -> Result<Array> {
-    Array::repeat_device::<T>(array, count, axis, stream)
+    Array::repeat_axis_device::<T>(array, count, axis, stream)
 }
 
-/// See [`Array::repeat_all`]
+/// See [`Array::repeat`]
 #[generate_macro]
 #[default_device]
-pub fn repeat_all_device<T: ArrayElement>(
+pub fn repeat_device<T: ArrayElement>(
     array: Array,
     count: i32,
     #[optional] stream: impl AsRef<Stream>,
 ) -> Result<Array> {
-    Array::repeat_all_device::<T>(array, count, stream)
+    Array::repeat_device::<T>(array, count, stream)
 }
 
 /// See [`Array::tri`]
@@ -700,7 +700,7 @@ mod tests {
     #[test]
     fn test_repeat() {
         let source = Array::from_slice(&[0, 1, 2, 3], &[2, 2]);
-        let array = Array::repeat::<i32>(source, 4, 1).unwrap();
+        let array = Array::repeat_axis::<i32>(source, 4, 1).unwrap();
         assert_eq!(array.shape(), &[2, 8]);
         assert_eq!(array.dtype(), Dtype::Int32);
 
@@ -711,18 +711,18 @@ mod tests {
     #[test]
     fn test_repeat_try() {
         let source = Array::from_slice(&[0, 1, 2, 3], &[2, 2]);
-        let array = Array::repeat::<i32>(source, 4, 1);
+        let array = Array::repeat_axis::<i32>(source, 4, 1);
         assert!(array.is_ok());
 
         let source = Array::from_slice(&[0, 1, 2, 3], &[2, 2]);
-        let array = Array::repeat::<i32>(source, -1, 1);
+        let array = Array::repeat_axis::<i32>(source, -1, 1);
         assert!(array.is_err());
     }
 
     #[test]
     fn test_repeat_all() {
         let source = Array::from_slice(&[0, 1, 2, 3], &[2, 2]);
-        let array = Array::repeat_all::<i32>(source, 4).unwrap();
+        let array = Array::repeat::<i32>(source, 4).unwrap();
         assert_eq!(array.shape(), &[16]);
         assert_eq!(array.dtype(), Dtype::Int32);
 
@@ -733,11 +733,11 @@ mod tests {
     #[test]
     fn test_repeat_all_try() {
         let source = Array::from_slice(&[0, 1, 2, 3], &[2, 2]);
-        let array = Array::repeat_all::<i32>(source, 4);
+        let array = Array::repeat::<i32>(source, 4);
         assert!(array.is_ok());
 
         let source = Array::from_slice(&[0, 1, 2, 3], &[2, 2]);
-        let array = Array::repeat_all::<i32>(source, -1);
+        let array = Array::repeat::<i32>(source, -1);
         assert!(array.is_err());
     }
 
